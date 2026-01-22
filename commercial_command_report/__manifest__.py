ecbf4f2e3287c4da06bdb24908badc71dad5b784
Micaela Ortiz <micaela@oncompetence.com>
2026-01-22 15:15:41 -0300

    [ADD] commercial_command_report

---
+++ commercial_command_report/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Cuadro de mando comercial",
+    "version": "18.0",
+    "author": "Tiago Rodriguez",
+    "depends": ["sale_management", "contacts"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/sale_report_sql_views.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
