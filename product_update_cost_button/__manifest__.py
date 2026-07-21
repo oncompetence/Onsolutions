22d299f2bb929ab62e1b02f75cb0d5335966e212
Gonzalo Fonseca <gonzalo@oncompetence.com>
2026-07-21 18:00:07 -0300

    [ADD] módulo que añade botón de actualizar costo contable en la vista form de producto

---
+++ product_update_cost_button/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Product Cost Update Button',
+    'version': '18.0.1.0.0',
+    'category': 'Inventory/Inventory',
+    'license': 'AGPL-3',
+    'author': 'Gonzalo Fonseca',
+    'summary': 'OnSolutions',
+    'description': 'Agrega un botón de acceso rápido para actualizar el costo contable.',
+    'depends': ['product', 'product_replenishment_cost'],
+    'data': [
+        'views/product_template_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
